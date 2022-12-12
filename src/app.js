7ae4db0c108c0bade55ff07b86f7a123e095a6f6
rasyadsr <m.rasyad.cr7@gmail.com>
2022-12-12 18:45:24 +0700

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,38 @@
 import express from "express";
+/**
+ * library untuk memudahkan layouting menggunakan ejs
+ * jadi ga perlu include header / footer di tiap halaman
+ * konsep nya seperti @extends dan @yields pada blade laravel
+ */
+import expressEjsLayouts from "express-ejs-layouts";
 
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.use(expressEjsLayouts);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  const mahasiswa = [
+    {
+      nama: "rasyad",
+      email: "rasyad@gmail.com",
+    },
+    {
+      nama: "erik",
+      email: "erik@gmail.com",
+    },
+    {
+      nama: "ricardo",
+      email: "ricardo@gmail.com",
+    },
+  ];
+  res.render("index", {
+    nama: "Mochamad Rasyad",
+    mahasiswa,
+    layout: "layouts/main-layout", // ini untuk memberi tahu layout yang digunakan
+    title: "Index",
+  });
 });
 
 app.get("/:id", (req, res) => {
